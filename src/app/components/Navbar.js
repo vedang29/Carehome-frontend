bb40f768eaad8e94e33d0b102e70767d7856a8fb
vedang29 <vedang294@gmail.com>
2025-03-19 16:55:38 +0530

    Initial commit - Carehome frontend setup

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,27 @@
+import { TbHome } from "react-icons/tb";
+import { LuHeartHandshake } from "react-icons/lu";
+import { CgInfo } from "react-icons/cg";
+import { MdAttachMoney } from "react-icons/md";
+import { PiSignInBold } from "react-icons/pi";
+
+import { IoLogoBuffer } from "react-icons/io5";
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center p-5'>
+        <div className='flex justify-center items-center gap-2'>
+            <IoLogoBuffer size={28}/>
+            <span>Logo</span>
+        </div>
+        <ul className='flex gap-8'>
+            <li className='flex gap-2 items-center'><TbHome size={28} />Home</li>
+            <li className='flex gap-2 items-center'><LuHeartHandshake size={28} />Services</li>
+            <li className='flex gap-2 items-center'><CgInfo size={28}/>About Us</li>
+            <li className='flex gap-2 items-center'><MdAttachMoney size={28} />Pricing & Plans</li>
+            <li className='flex gap-2 items-center'><PiSignInBold size={28}/>Sign In</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
