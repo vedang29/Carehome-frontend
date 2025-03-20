c8d48b9c744511404768f51087279a179cb394ac
ManavKokne <koknemanav@gmail.com>
2025-03-20 13:56:10 +0530

    Added Footer and Fixed SideBar

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,24 +1,34 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/landing/Footer";
 import ListingCard from "../components/listing/ListingCard";
 import SideBar from "../components/listing/SideBar";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl min-h-screen mx-auto">
-      <Navbar />
-      <div className="py-8 space-y-6 flex flex-row space-x-5 justify-center align-middle">
-        <div className="space-y-6">
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
-          <ListingCard />
+    <>
+      <div className="max-w-7xl min-h-screen mx-auto">
+        <Navbar />
+
+        <div className="py-8 space-y-6 flex flex-row space-x-5 justify-center align-middle">
+
+          <div className="space-y-10 mb-0">
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+            <ListingCard />
+          </div>
+
+          <div className="sticky top-0 h-fit">
+            <SideBar />
+          </div>
+
         </div>
-        <SideBar />
-     </div>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 }
